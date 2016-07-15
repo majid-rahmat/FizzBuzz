fecd7abb966b766c55d64494eeea6d1a5a571b54
majid-rahmat <majid.rahmat@gmail.com>
2016-07-14 20:07:49 -0400

    empty

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-$(document).ready(function(){
-  function appendText() {
-  for (var i = 1; i <= 100; i++) {
-      
-      if ((i % 3 === 0) && (i % 5 === 0)) {
-    $("ul").append('<li>fizzbuzz</li>');
-  } 
-  else if (i % 3 === 0) {
-    $("ul").append('<li>fizz</li>');
-  }  
-  else if (i % 5 === 0) {
-    $("ul").append('<li>buzz</li>');
-  }
-  else {
-    $("ul").append('<li>' + i + '</li>');
-  }
-}
-}
+$(document).ready(function() {
 
-  appendText();
+    $('button').click(function() {
+        validate($('input').val());
+    });
 
+
+}).keypress(function(e) {
+
+    if (e.which === 13) {
+        validate($('input').val());
+    }
 });
+
+
+function validate(input) {
+    if (isNaN(input)) {
+        return alert('Please enter a valid number!');
+    } else {
+        fizzbuzz(input);
+    }
+}
+
+function fizzbuzz(input) {
+    $('ul').empty();
+    for (var i = 1; i <= input; i++) {
+        if ((i % 3 === 0) && (i % 5 === 0)) {
+            $("ul").append('<li>fizzbuzz</li>');
+        } else if (i % 3 === 0) {
+            $("ul").append('<li>fizz</li>');
+        } else if (i % 5 === 0) {
+            $("ul").append('<li>buzz</li>');
+        } else {
+            $("ul").append('<li>' + i + '</li>');
+        }
+    }
+}
